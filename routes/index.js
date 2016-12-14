2fb31e196ac4b5afc20500c80d7c378ee27e7fbd
jeremyclayton <jeremy.clayton.91@gmail.com>
2016-12-14 15:17:13 -0700

    finsihed put request

--- routes/index.js
+++ routes/index.js
@@ -34,40 +34,39 @@ router.post('/new', function(req, res, next) {
         res.redirect('/snowboard/' + id);
     });
 });
-// router.delete('snowboard/:id', (req,res)=>){
-//     let id = req.params.id;
-//      knex('snowboard')
-//      .where('id', id)
-//              .del()
-//              .then(function (){
-//                  res.redirect('/snowboard');
-//              });
-// });
-// router.get('/delete', function(req, res, next) {
-//     res.render('delete');
-// });
+
 router.delete('/snowboard/:id', (req, res) =>{
-    // console.log('snowboard');
     let id = req.params.id;
     knex('snowboard')
         .where('id', id)
         .del()
         .then(function (){
-            res.redirect('/snowboard');
+            res.redirect('/snowboard/');
         });
 });
-// router.delete('/:id', (req, res) => {
-//   knex ("snowboard").del({
-//       make: req.body.make,
-//       model: req.body.model,
-//       size: req.body.size,
-//       price: req.body.price,
-//       flex: req.body.flex,
-//   })
-//   .then(function () {
-//     res.redirect('/snowboard');
-//     console.log(id);
-// });
-// });
+router.get('/snowboard/:id/edit', function(req, res, next) {
+    knex('snowboard')
+        .where('id', req.params.id)
+        .then((data) => {
+            res.render('edit', {
+                data: data
+            });
 
+        });
+});
+
+router.put('/snowboard/:id', (req, res) =>{
+    let id = req.params.id;
+    knex('snowboard')
+        .where('id', id)
+        .update({
+            make: req.body.make,
+            model: req.body.model,
+            size: req.body.size,
+            price: req.body.price,
+            flex: req.body.flex
+        }, 'id').then(function (){
+            res.redirect('/snowboard/' + id);
+        });
+});
 module.exports = router;
